4be6225ede69c547bf7d57f91f332004374addd7
clong20137 <83474031+clong20137@users.noreply.github.com>
2021-10-17 20:04:38 -0400

    Updates

--- src/App.js
+++ src/App.js
@@ -1,62 +1,97 @@
 import React, {useState } from 'react';
+import ReactDOM from 'react-dom'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle';
 import './App.css';
+import './index.js';
+
+
 
 const App = () => {
-    const [result, setResult] = useState("");
+    
+    const [result, setResult] = useState(""); 
 
+    const LogThis = () => {
+        console.log(result);
+      
 
-  
-  
-    const handleClick = (e) => { // USE NAME VALUE FOR CALCULATOR
-        setResult(result.concat(e.target.name));
     }
+    const handleClick = (e) => { // USE NAME VALUE FOR CALCULATOR
+       
+            setResult(result.concat(e.target.name));
+           
+    }
+
     const ac = () => { // ALL CLEAR 
         setResult("");
+      
     }
     const backspace = () => { // DELETE LAST INPUT
         setResult(result.slice(0, -1));
+       
 
     }
     const log = () => { // LETS LOG THE INPUT
 
     }
 
-
    const equal = () => {
        try {
         setResult(eval(result).toString());
+        LogThis();
        } catch (err) {
-           setResult("Error");
+           setResult("ERROR!");
+           LogThis();
+
+           
        }
-     
-   
-   
+
+       
+      
+ 
 }
 
     return (
-     
-        <div className="calculator">
-<div className="topbar">
-  
 
-<svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" fill="white" class="bi bi-list" viewBox="0 0 16 16">
-  <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
-</svg>
+        <div className="calculator">
+            
+        <div className="topbar">
+            
+<div id="myModal" class="modal">
+
+  
+<div class="modal-content">
+  <span class="close">&times;</span>
+  <h6>Recent Calculations</h6>
+  <p>Coming soon.</p>
 </div>
 
+</div>
 
-            <form>
-                <input type="text" value={result} disabled />
-            </form>
+        <li class="navbar navbar-light">
+        <a class="nav-link dropdown" href="#" id="navbarScrollingDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+        <svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" fill="white" class="bi bi-list" viewBox="0 0 16 16">
+        <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
+        </svg>
+        </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarScrollingDropdown">
+            <li><a class="dropdown-item" href="#">Conversion Tool</a></li>
+            <li><a class="dropdown-item" href="#" id="myBtn">Calculator Log</a></li>
+          </ul>
+        </li> 
+        </div>
 
+
+
+        <form>
+        <input type="text" name="input" id="input" value={result} disabled />
+        </form>
+        
             <div className="inputKeys">
             <button onClick={backspace} id="delete" class="buttonKey-yellow">C</button>
             <button onClick={ac} id="ac" class="buttonKey-yellow">AC</button>
             <button onClick={handleClick} id="divide" name="/" class="buttonKey">&divide;</button>
             <button onClick={handleClick} name="*" class="buttonKey">&times;</button>
-                
                 <button onClick={handleClick} name="7" class="buttonKey-white">7</button>
                 <button onClick={handleClick} name="8" class="buttonKey-white">8</button>
                 <button onClick={handleClick} name="9" class="buttonKey-white">9</button>
@@ -75,9 +110,12 @@ const App = () => {
                 <div class="empty"></div>
                 <button onClick={equal} id="equal" class="buttonKey">=</button>
             </div>
-            
+          
 
-</div>
+
+
+            </div>
+
 
         
     )
--- src/index.js
+++ src/index.js
@@ -3,3 +3,29 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'))
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
